a42edae882bf070d4061da789f979863ec688523
s0mone <121167607+s0mone@users.noreply.github.com>
2022-12-21 18:48:08 +0330

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,13 @@ async def on_message(message):
     #it checks if the bot itself sent the message and will ignore it
     if message.author == client.user:
         return
+
+url = ('https://newsapi.org/v2/everything?'
+       'q=Apple&'
+       'from=2022-12-21&'
+       'sortBy=popularity&'
+       'apiKey=API_KEY')
+
+response = requests.get(url)
+
+print r.json
