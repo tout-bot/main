8eadcad53739773e5c6b61764d582494ec6b7df9
s0mone <121167607+s0mone@users.noreply.github.com>
2022-12-21 15:34:06 +0330

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'Successfully logged in as {client.user}')
+
+#if ur not going to add commands then remove this
+@client.event
+async def on_message(message):
+    # Ignore messages from the bot itself
+    if message.author == client.user:
+        return
